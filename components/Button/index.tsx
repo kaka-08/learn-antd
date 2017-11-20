cbd4c888f857bb24b9c47f7a4129bce7e5f4bf09
kaka_08 <602993561@qq.com>
2017-11-20 16:58:50 +0800

    123

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,3 +1,13 @@
-import Button from './Button';
+import React,{Components} from "react";
+
+class Button extends Components{
+
+  render(){
+    return(
+      <p>this is button</p>
+    )
+  }
+
+}
 
 export default Button;
